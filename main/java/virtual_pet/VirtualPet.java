fb4b5bf6d920d2dd5adb92a5e25998213a25d94e
Amani Zayed <133261261+A-Zayed@users.noreply.github.com>
2023-07-21 11:36:45 -0500

    Add files via upload

---
+++ main/java/virtual_pet/VirtualPet.java
@@ -0,0 +1,10 @@
+package virtual_pet;
+
+public class VirtualPet {
+    public static void main(String[] args) {
+        VirtualPet pet = new VirtualPet("name");
+        pet.feed();
+        pet.play();
+
+    }
+}
